52b3ae8389bb73036048f3eba37dd3370e948e97
Joseph Olawale Bamidele <47855279+jolawale28@users.noreply.github.com>
2025-05-08 19:07:27 +0100

    fix: fonts, inline styles
    - formatted for backend engineers

--- src/components/modules/DashboardModuleCard.jsx
+++ src/components/modules/DashboardModuleCard.jsx
@@ -1,5 +1,3 @@
-import { FeatureUpgradeModal } from '../ui/Modals'
-
 export default function DashboardModuleCard ({
   title,
   subtitle,
@@ -7,32 +5,15 @@ export default function DashboardModuleCard ({
   href,
   color
 }) {
-  const handleClick = evt => {
-    evt.preventDefault()
-    if (href != '#') {
-      window.open(href, title)
-    } else {
-      const myModal = new bootstrap.Modal('#showUpgradeModal', {
-        keyboard: true
-      })
-      myModal.show()
-    }
-  }
-
   return (
     <>
       <a
         href={href}
         title={title}
-        onClick={handleClick}
         className='col-3 p-0 inline-block text-decoration-none min-w-400 dashboard_module_card'
       >
         <div
-          className='rounded-4 py-4 px-5 d-flex h-100 flex-column align-items-center justify-content-start gap-2 text-center'
-          style={{
-            border: `10px solid ${color}`,
-            boxShadow: '4px 4px 10px 0px #00000040'
-          }}
+          className={`rounded-4 py-4 px-5 d-flex h-100 flex-column align-items-center justify-content-start gap-2 text-center ${color}`}
         >
           <div>
             <img src={icon} alt={title} />
@@ -41,8 +22,6 @@ export default function DashboardModuleCard ({
           <div className='fw-semibold fs-6 text-black'>{subtitle}</div>
         </div>
       </a>
-
-      {href == '#' && <FeatureUpgradeModal />}
     </>
   )
 }
