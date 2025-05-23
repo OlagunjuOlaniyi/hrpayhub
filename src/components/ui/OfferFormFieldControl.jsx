d16f9b290d6e097e6473eba3c94daf9cabca7fed
OlagunjuOlaniyi <olagunjuolaniyi512@gmail.com>
2025-05-23 15:45:59 +0100

    update: create offer breakdown page

--- src/components/ui/OfferFormFieldControl.jsx
+++ src/components/ui/OfferFormFieldControl.jsx
@@ -1,16 +1,16 @@
 export const FormFieldComponent = (props) => {
-  const { label, index } = props;
+  const { className, label, index } = props;
   return (
     <>
-      <div className="form-floating col-md-8 col-12 p-0">
+      <div className={`form-floating col-md-8 col-12 p-0 ${className}`}>
         <input
-          type="text"
+          type="number"
           id={`ilugyhdg_${index}`}
           name={label.replaceAll(" ", "")}
           className="form-input deductionInputFields"
           placeholder=" "
         />
-        <label htmlFor="date" className="ms-1">
+        <label htmlFor="date" className="ms-1 fs-12">
           {label} &nbsp;<span>*</span>
         </label>
       </div>
