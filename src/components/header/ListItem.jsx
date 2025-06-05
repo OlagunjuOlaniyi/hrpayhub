3764e07481fd64ba3a85cbee12cc1e90e599a197
OlagunjuOlaniyi <olagunjuolaniyi512@gmail.com>
2025-06-05 15:30:51 +0100

    update:  national holiday fixes

--- src/components/header/ListItem.jsx
+++ src/components/header/ListItem.jsx
@@ -1,13 +1,15 @@
 export const DropdownListItem = ({ label, icon }) => {
-
   return (
-    <div className="" id = {label.replaceAll(' ', '').toLowerCase()}>
-      <a className='dropdown-item d-flex gap-3 align-items-center py-2 d-inline-block fs-6' href={`/${label.replaceAll(' ', '_').toLowerCase()}.html`}>
+    <div className="" id={label.replaceAll(" ", "").toLowerCase()}>
+      <a
+        className="dropdown-item d-flex gap-3 align-items-center py-2 d-inline-block fs-6"
+        href={`/${label.replaceAll(" ", "-").toLowerCase()}.html`}
+      >
         <div className="d-flex align-items-center justify-content-center w-22">
-          <img src = {icon} alt = {label} />
+          <img src={icon} alt={label} />
         </div>
         <div className="list-item-label">{label}</div>
       </a>
     </div>
-  )
-}
+  );
+};
