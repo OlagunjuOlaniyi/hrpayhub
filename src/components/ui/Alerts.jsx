36a075e05324d1cd1baa32bf5ae73ed9b6edfb52
OlagunjuOlaniyi <olagunjuolaniyi512@gmail.com>
2025-06-08 18:12:26 +0100

    update: on offboarding page

--- src/components/ui/Alerts.jsx
+++ src/components/ui/Alerts.jsx
@@ -1,26 +1,38 @@
+import { useState } from "react";
+
 export const AlertComponent = () => {
+  const [showAlert, setShowAlert] = useState(true);
+
   return (
-    <section className="alertNotice">
-      <div className="screenFrame">
-        <div className="mx-4 p-3 rounded-2 d-flex align-items-start justify-content-between gap-3 notice-board">
-          <div>
-            <img
-              src="images/png/icons/colored/warning-2-colored.svg"
-              alt="warning-cion"
-            />
-          </div>
-          <div className="vstack gap-2">
-            <div className="fw-bold text-red fs-16">Missing Details</div>
-            <div className="text-red fs-14">
-              Complete profile, employment as if you are the one and department
-              setup to proceed.
+    <>
+      {showAlert && (
+        <section className="alertNotice">
+          <div className="screenFrame">
+            <div className="mx-4 p-3 rounded-2 d-flex align-items-start justify-content-between gap-3 notice-board">
+              <div>
+                <img
+                  src="images/png/icons/colored/warning-2-colored.svg"
+                  alt="warning-cion"
+                />
+              </div>
+              <div className="vstack gap-2">
+                <div className="fw-bold text-red fs-16">Missing Details</div>
+                <div className="text-red fs-14">
+                  Complete profile, employment as if you are the one and
+                  department setup to proceed.
+                </div>
+              </div>
+
+              <button
+                className="bg-transparent border-0 p-0 m-0 closeButton"
+                onClick={() => setShowAlert(false)}
+              >
+                <img src="images/png/icons/colored/x.svg" alt="close-icon" />
+              </button>
             </div>
           </div>
-          <button className="bg-transparent border-0 p-0 m-0 closeButton">
-            <img src="images/png/icons/colored/x.svg" alt="close-icon" />
-          </button>
-        </div>
-      </div>
-    </section>
+        </section>
+      )}
+    </>
   );
 };
