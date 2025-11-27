26dd53d287c954097706dd0279c1311ca7010827
OlagunjuOlaniyi <olagunjuolaniyi512@gmail.com>
2025-11-27 22:23:03 +0100

    update: timezone on branch tab and style touch up

---
+++ src/components/ui/TimeZones.jsx
@@ -0,0 +1,73 @@
+export default function () {
+  return (
+    <>
+      <option disabled selected>
+        --Select Time Zone--
+      </option>
+
+      <option value="Pacific/Midway">(UTC-11:00) Midway Island</option>
+      <option value="Pacific/Honolulu">(UTC-10:00) Hawaii</option>
+      <option value="America/Anchorage">(UTC-09:00) Alaska</option>
+      <option value="America/Los_Angeles">
+        (UTC-08:00) Pacific Time (US & Canada)
+      </option>
+      <option value="America/Denver">
+        (UTC-07:00) Mountain Time (US & Canada)
+      </option>
+      <option value="America/Phoenix">(UTC-07:00) Arizona</option>
+      <option value="America/Chicago">
+        (UTC-06:00) Central Time (US & Canada)
+      </option>
+      <option value="America/Mexico_City">(UTC-06:00) Mexico City</option>
+      <option value="America/New_York">
+        (UTC-05:00) Eastern Time (US & Canada)
+      </option>
+      <option value="America/Toronto">(UTC-05:00) Eastern Time (Canada)</option>
+      <option value="America/Bogota">(UTC-05:00) Bogotá</option>
+      <option value="America/Caracas">(UTC-04:00) Caracas</option>
+      <option value="America/Santiago">(UTC-04:00) Santiago</option>
+      <option value="America/St_Johns">(UTC-03:30) Newfoundland</option>
+      <option value="America/Buenos_Aires">(UTC-03:00) Buenos Aires</option>
+      <option value="America/Sao_Paulo">(UTC-03:00) São Paulo</option>
+      <option value="Atlantic/South_Georgia">(UTC-02:00) South Georgia</option>
+      <option value="Atlantic/Azores">(UTC-01:00) Azores</option>
+      <option value="Europe/London">(UTC+00:00) London</option>
+      <option value="Africa/Accra">(UTC+00:00) Accra</option>
+      <option value="Africa/Lagos">(UTC+01:00) Lagos</option>
+      <option value="Europe/Berlin">(UTC+01:00) Central European Time</option>
+      <option value="Europe/Paris">(UTC+01:00) Paris</option>
+      <option value="Africa/Cairo">(UTC+02:00) Cairo</option>
+      <option value="Africa/Johannesburg">(UTC+02:00) Johannesburg</option>
+      <option value="Europe/Athens">(UTC+02:00) Athens</option>
+      <option value="Asia/Jerusalem">(UTC+02:00) Jerusalem</option>
+      <option value="Europe/Moscow">(UTC+03:00) Moscow</option>
+      <option value="Africa/Nairobi">(UTC+03:00) Nairobi</option>
+      <option value="Asia/Tehran">(UTC+03:30) Tehran</option>
+      <option value="Asia/Dubai">(UTC+04:00) Dubai</option>
+      <option value="Asia/Baku">(UTC+04:00) Baku</option>
+      <option value="Asia/Kabul">(UTC+04:30) Kabul</option>
+      <option value="Asia/Karachi">(UTC+05:00) Karachi</option>
+      <option value="Asia/Tashkent">(UTC+05:00) Tashkent</option>
+      <option value="Asia/Kolkata">(UTC+05:30) India Standard Time</option>
+      <option value="Asia/Kathmandu">(UTC+05:45) Kathmandu</option>
+      <option value="Asia/Dhaka">(UTC+06:00) Dhaka</option>
+      <option value="Asia/Yangon">(UTC+06:30) Yangon</option>
+      <option value="Asia/Bangkok">(UTC+07:00) Bangkok</option>
+      <option value="Asia/Jakarta">(UTC+07:00) Jakarta</option>
+      <option value="Asia/Hong_Kong">(UTC+08:00) Hong Kong</option>
+      <option value="Asia/Shanghai">(UTC+08:00) Beijing</option>
+      <option value="Asia/Singapore">(UTC+08:00) Singapore</option>
+      <option value="Australia/Perth">(UTC+08:00) Perth</option>
+      <option value="Asia/Tokyo">(UTC+09:00) Tokyo</option>
+      <option value="Asia/Seoul">(UTC+09:00) Seoul</option>
+      <option value="Australia/Adelaide">(UTC+09:30) Adelaide</option>
+      <option value="Australia/Darwin">(UTC+09:30) Darwin</option>
+      <option value="Australia/Sydney">(UTC+10:00) Sydney</option>
+      <option value="Australia/Brisbane">(UTC+10:00) Brisbane</option>
+      <option value="Pacific/Guam">(UTC+10:00) Guam</option>
+      <option value="Pacific/Fiji">(UTC+12:00) Fiji</option>
+      <option value="Pacific/Auckland">(UTC+12:00) Auckland</option>
+      <option value="Pacific/Tongatapu">(UTC+13:00) Tonga</option>
+    </>
+  );
+}
